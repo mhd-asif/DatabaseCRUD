0db11d8e62b1091dd687da75549ed36c5914d313
nasir-PC\asif <mhd.asif.1988@gmail.com>
2017-01-08 18:50:55 +0600

    Added buttons

--- app/src/main/java/com/example/asif/databasecrud/MainActivity.java
+++ app/src/main/java/com/example/asif/databasecrud/MainActivity.java
@@ -2,14 +2,21 @@ package com.example.asif.databasecrud;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<String> menulist = new ArrayList<>();
+    Button btnAdd;
+    Button btnSearch;
+    Button btnUpdate;
+    Button btnDelete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +32,39 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayAdapter<String> arrAdapter= new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menulist);
         listview.setAdapter(arrAdapter);
+
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Add button clicked", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        btnSearch = (Button) findViewById(R.id.btnSearch);
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnUpdate = (Button) findViewById(R.id.btnUpdate);
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnDelete = (Button) findViewById(R.id.btnDelete);
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
 
     }
 }
